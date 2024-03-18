c6db64743cda8e7076a873fb9a4d281c71062f81
Gabriel13007 <98387568+Gabriel13007@users.noreply.github.com>
2024-03-18 14:59:24 +0100

    2e commit
    Pas bcp de changement

--- MonJoueur.java
+++ MonJoueur.java
@@ -1,8 +1,11 @@
 package main;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import gui.Point;
 import jeu.Joueur;
@@ -21,73 +24,75 @@ public class MonJoueur extends Joueur {
 		super(sonNom);
 	}
 
-	// Utile : plateau.donneStocksDesFabriques()
+	
 	@Override
 	public Action faitUneAction(Plateau etatDuJeu) {
 		plateau = etatDuJeu;
 		if (this.donneRessources() < 451) {
-			if (plateau.contientUneColline(plateau.donneContenuCellule(this.donnePosition()))) {
-				return Action.RIEN;
-			} else {
-				int plateauTaille = plateau.donneTaille();
-				for (int i = 1; i < plateauTaille; i++) {
-					HashMap<Integer, ArrayList<Point>> mapCollineProche = plateau.cherche(this.donnePosition(), i,
-							Plateau.CHERCHE_COLLINE);
-					if (!mapCollineProche.get(1).isEmpty()) {
-						HashMap<Integer, ArrayList<Point>> mapObstacle = plateau.cherche(this.donnePosition(), i,
-								Plateau.CHERCHE_TOUT);
-						ArrayList<Node> pcc = plateau.donneCheminAvecObstaclesSupplementaires(this.donnePosition(),
-								mapCollineProche.get(1).get(0), convertToNode(mapObstacle, false, true, true));
-						return moveTo(pcc.get(0));
-					}
-				}
+			Action action = moveToColline();
+			if(action != null) {
+				return action;
 			}
 		} else {
-			int plateauTaille = plateau.donneTaille();
-			for (int i = 1; i < plateauTaille; i++) {
-				HashMap<Integer, ArrayList<Point>> mapFabriqueProche = plateau.cherche(this.donnePosition(), i,
-						Plateau.CHERCHE_FABRIQUE);
-				if (!mapFabriqueProche.get(2).isEmpty()) {
-					HashMap<Integer, ArrayList<Point>> mapObstacle = plateau.cherche(this.donnePosition(), i,
-							Plateau.CHERCHE_TOUT);
-					ArrayList<Node> pcc = plateau.donneCheminAvecObstaclesSupplementaires(this.donnePosition(),
-							mapFabriqueProche.get(2).get(0), convertToNode(mapObstacle, true, false, true));
-					pcc.toString();
-					if(!pcc.isEmpty()) return moveTo(pcc.get(0));
-				}
+			Action action = moveToFabrique();
+			if(action != null) {
+				return action;
 			}
 		}
 		return super.faitUneAction(plateau); // a modifier
 	}
 
+	@SuppressWarnings("static-access")
 	public List<Node> convertToNode(HashMap<Integer, ArrayList<Point>> map, boolean includeColline,
 			boolean includeFabrique, boolean includeJoueur) {
 		List<Node> lst = new ArrayList<>();
 
 		if (includeColline) {
 			ArrayList<Point> lstColline = map.get(1);
-			for (int i = 0; i < lstColline.size(); i++) {
-				Point colline = lstColline.get(i);
+			for (Point colline : lstColline) {
 				lst.add(new Node(colline.x, colline.y));
 			}
 		}
 
 		if (includeFabrique) {
 			ArrayList<Point> lstFabrique = map.get(2);
-			for (int i = 0; i < lstFabrique.size(); i++) {
-				Point fabrique = lstFabrique.get(i);
+			for (Point fabrique : lstFabrique) {
 				lst.add(new Node(fabrique.x, fabrique.y));
+			}
+		}else {
+			ArrayList<Point> lstFabrique = map.get(2);
+			for (Point fabrique : lstFabrique) {
+				if(!estMaFabrique(fabrique)
+						|| estRempli(fabrique)) {
+					lst.add(new Node(fabrique.x, fabrique.y));
+				}
 			}
 		}
 
 		if (includeJoueur) {
 			ArrayList<Point> lstJoueur = map.get(4);
-			for (int i = 0; i < lstJoueur.size(); i++) {
-				Point joueur = lstJoueur.get(i);
+			for (Point joueur : lstJoueur) {
 				lst.add(new Node(joueur.x, joueur.y));
 			}
 		}
 		return lst;
+	}
+
+	private boolean estRempli(Point fabrique) {
+		Map<Point, Integer> mapStock = plateau.donneStocksDesFabriques();
+		Integer stock = mapStock.get(fabrique);
+		return stock < nbTourRestant();
+	}
+	
+	private boolean estVide(Point fabrique) {
+		Map<Point, Integer> mapStock = plateau.donneStocksDesFabriques();
+		Integer stock = mapStock.get(fabrique);
+		return stock == null || stock == 0;
+	}
+	
+	@SuppressWarnings("static-access")
+	private boolean estMaFabrique(Point fabrique) {
+		return (donneRang()+1) == plateau.donneUtilisateurDeLaFabrique(plateau.donneContenuCellule(fabrique.x, fabrique.y));
 	}
 
 	private Action moveTo(Node node) {
@@ -107,8 +112,67 @@ public class MonJoueur extends Joueur {
 	@Override 
 	public int donneRessourcesPourFabrique(Point p){
 		if(plateau != null) {
-			return donneRessources() - (plateau.donneNombreDeTours() - plateau.donneTourCourant());
+			Map<Point, Integer> mapStock = plateau.donneStocksDesFabriques();
+			Integer stock = mapStock.get(p);
+			if(stock != null && nbTourRestant()-stock<donneRessources()) {
+				return donneRessources() - stock - nbTourRestant();
+			}
 		}
 		return donneRessources();
+	}
+	
+	private int nbTourRestant() {
+		return (plateau.donneNombreDeTours() - plateau.donneTourCourant());
+	}
+	
+	@SuppressWarnings("static-access")
+	private Action moveToColline() {
+		if (plateau.contientUneColline(plateau.donneContenuCellule(this.donnePosition()))) {
+			return Action.RIEN;
+		} else {
+			int plateauTaille = plateau.donneTaille();
+			for (int i = 1; i < plateauTaille; i++) {
+				HashMap<Integer, ArrayList<Point>> mapCollineProche = plateau.cherche(this.donnePosition(), i,
+						Plateau.CHERCHE_COLLINE);
+				if (!mapCollineProche.get(1).isEmpty()) {
+					HashMap<Integer, ArrayList<Point>> mapObstacle = plateau.cherche(this.donnePosition(), i,
+							Plateau.CHERCHE_TOUT);
+					ArrayList<Node> pcc = plateau.donneCheminAvecObstaclesSupplementaires(this.donnePosition(),
+							mapCollineProche.get(1).get(0), convertToNode(mapObstacle, false, true, true));
+					if(pcc != null) return moveTo(pcc.get(0));
+				}
+			}
+		}
+		return null;
+	}
+	
+	@SuppressWarnings("static-access")
+	private Action moveToFabrique() {
+		int plateauTaille = plateau.donneTaille();
+		for (int i = 1; i < plateauTaille; i++) {
+			HashMap<Integer, ArrayList<Point>> mapFabriqueProche = plateau.cherche(this.donnePosition(), i,
+					Plateau.CHERCHE_FABRIQUE);
+			if (!mapFabriqueProche.get(2).isEmpty()) {
+				for(Point fabrique : mapFabriqueProche.get(2)) {
+					if(estVide(fabrique)) {
+						HashMap<Integer, ArrayList<Point>> mapObstacle = plateau.cherche(this.donnePosition(), i,
+								Plateau.CHERCHE_TOUT);
+						ArrayList<Node> pcc = plateau.donneCheminAvecObstaclesSupplementaires(this.donnePosition(),
+								mapFabriqueProche.get(2).get(0), convertToNode(mapObstacle, true, false, true));
+						pcc.toString();
+						if(!pcc.isEmpty()) return moveTo(pcc.get(0));
+					}
+					if(estMaFabrique(fabrique) &&  estRempli(fabrique)) {
+						HashMap<Integer, ArrayList<Point>> mapObstacle = plateau.cherche(this.donnePosition(), i,
+								Plateau.CHERCHE_TOUT);
+						ArrayList<Node> pcc = plateau.donneCheminAvecObstaclesSupplementaires(this.donnePosition(),
+								mapFabriqueProche.get(2).get(0), convertToNode(mapObstacle, true, false, true));
+						pcc.toString();
+						if(!pcc.isEmpty()) return moveTo(pcc.get(0));
+					}
+				}
+			}
+		}
+		return null;
 	}
 }
