54ad3e87fb22ced8c66a1856f95b2583d714ef24
Gabriel13007 <98387568+Gabriel13007@users.noreply.github.com>
2024-03-19 11:33:06 +0100

    ColonOptimise
    Recherche optimise pour la recherche de colline

--- ColonOptimise.java
+++ ColonOptimise.java
@@ -2,6 +2,7 @@ package main;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import gui.Point;
 import jeu.Plateau;
@@ -13,6 +14,66 @@ public class ColonOptimise extends MonJoueur{
 		super(sonNom);
 	}
 	
+	protected boolean estPresqueVide(Point fabrique) {
+		Map<Point, Integer> mapStock = plateau.donneStocksDesFabriques();
+		Integer stock = mapStock.get(fabrique);
+		return stock == null || stock <= 200;
+	}
+	
+	@Override
+	protected Action moveToColline() {
+		if (estSurColline()) {
+			return Action.RIEN;
+		} else {
+			ArrayList<Point> collineProche = plateau.cherche(this.donnePosition(), 500,
+					Plateau.CHERCHE_COLLINE).get(1);
+			ArrayList<Node> bestPcc = null;
+			for(Point colline : collineProche) {
+				int contenuCellule = plateau.donneContenuCellule(colline.x, colline.y);
+				if(!Plateau.contientUnJoueur(contenuCellule)) {
+					HashMap<Integer, ArrayList<Point>> mapObstacle = plateau.cherche(this.donnePosition(), 500,
+							Plateau.CHERCHE_FABRIQUE);
+					mapObstacle.putAll(plateau.cherche(this.donnePosition(), 500,
+							Plateau.CHERCHE_JOUEUR));
+					ArrayList<Node> pcc = plateau.donneCheminAvecObstaclesSupplementaires(this.donnePosition(),
+							colline, convertToNode(mapObstacle, false, true, true));
+					if(bestPcc == null && pcc != null || pcc != null && pcc.size()<bestPcc.size()) {
+						bestPcc = pcc;
+					}
+				}
+			}
+			if(bestPcc != null && bestPcc.get(0) != null) return moveTo(bestPcc.get(0));
+		}
+		return null;
+	}
+	
+	@Override 
+	public int donneRessourcesPourFabrique(Point p){
+		int don = 0;
+		if(plateau != null) {
+			Map<Point, Integer> mapStock = plateau.donneStocksDesFabriques();
+			Integer stock = mapStock.get(p);
+			int donMax;
+			if(stock != null) {
+				donMax = nbTourRestant() - stock;
+			} else {
+				donMax = nbTourRestant();
+			}
+			if(donMax < donneRessources()) {
+				don = donMax;
+			} else {
+				don = donneRessources();
+			}
+		}
+		
+		if(don != 0 && don <= 350) {
+			return don;
+		}else {
+			return 350;
+		}
+		//return donneRessources();
+	}
+	
 	@Override
 	protected Action moveToFabrique() {
 		HashMap<Integer, ArrayList<Point>> mapFabriqueProche = plateau.cherche(this.donnePosition(), 500,
@@ -20,7 +81,7 @@ public class ColonOptimise extends MonJoueur{
 		ArrayList<Node> bestPccVide = null;
 		ArrayList<Node> bestPcc = null;
 		for(Point fabrique : mapFabriqueProche.get(2)) {
-			if(estVide(fabrique)) {
+			if(estVide(fabrique) || estPresqueVide(fabrique)) {
 				HashMap<Integer, ArrayList<Point>> mapObstacle = plateau.cherche(this.donnePosition(), 500,
 						Plateau.CHERCHE_FABRIQUE);
 				mapObstacle.putAll(plateau.cherche(this.donnePosition(), 500,
@@ -31,7 +92,7 @@ public class ColonOptimise extends MonJoueur{
 					boolean ok = true;
 					for(Node node : pcc) {
 						int contenuCellule = plateau.donneContenuCellule(node.x, node.y); // joueur @1
-						if(contenuCellule == Plateau.ENDROIT_INFRANCHISSABLE) {
+						if(Plateau.contientUnJoueur(contenuCellule)) {
 							ok = false;
 						}
 					}
@@ -50,7 +111,7 @@ public class ColonOptimise extends MonJoueur{
 					boolean ok = true;
 					for(Node node : pcc) {
 						int contenuCellule = plateau.donneContenuCellule(node.x, node.y); // joueur @1
-						if(contenuCellule == Plateau.ENDROIT_INFRANCHISSABLE) {
+						if(Plateau.contientUnJoueur(contenuCellule)) {
 							ok = false;
 						}
 					}
