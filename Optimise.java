6bb25f938cbf097456b5a2615b73aacc24a99039
Gabriel13007 <98387568+Gabriel13007@users.noreply.github.com>
2024-03-19 09:31:56 +0100

    Joueur Optimisé
    Il n'esquive plus les collines et voit quand une colline est occupé par un joueur.

--- Optimise.java
+++ Optimise.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 
 import gui.Point;
 import jeu.Plateau;
+import jeu.Joueur.Action;
 import jeu.astar.Node;
 
 public class Optimise extends MonJoueur{
@@ -13,6 +14,40 @@ public class Optimise extends MonJoueur{
 		super(sonNom);
 		// TODO Auto-generated constructor stub
 	}
+	
+//	@Override
+//	protected Action moveToColline() {
+//		if (estSurColline()) {
+//			return Action.RIEN;
+//		} else {
+//			HashMap<Integer, ArrayList<Point>> mapCollineProche = plateau.cherche(this.donnePosition(), 500,
+//					Plateau.CHERCHE_COLLINE);
+//			ArrayList<Node> bestPcc = null;
+//			
+//			for(Point colline : mapCollineProche.get(1)) {
+//				ArrayList<Node> pcc = plateau.donneCheminAvecObstaclesSupplementaires(this.donnePosition(),
+//						mapCollineProche.get(1).get(0), convertToNode(mapObstacle, false, true, true));
+//				for(Node node : pcc) {
+//					
+//				}
+//			}
+//			
+//			
+//			int plateauTaille = plateau.donneTaille();
+//			for (int i = 1; i < plateauTaille; i++) {
+//				HashMap<Integer, ArrayList<Point>> mapCollineProche = plateau.cherche(this.donnePosition(), i,
+//						Plateau.CHERCHE_COLLINE);
+//				if (!mapCollineProche.get(1).isEmpty()) {
+//					HashMap<Integer, ArrayList<Point>> mapObstacle = plateau.cherche(this.donnePosition(), i,
+//							Plateau.CHERCHE_TOUT);
+//					ArrayList<Node> pcc = plateau.donneCheminAvecObstaclesSupplementaires(this.donnePosition(),
+//							mapCollineProche.get(1).get(0), convertToNode(mapObstacle, false, true, true));
+//					if(pcc != null) return moveTo(pcc.get(0));
+//				}
+//			}
+//		}
+//		return null;
+//	}
 	
 	@Override
 	@SuppressWarnings("static-access")
@@ -23,14 +58,16 @@ public class Optimise extends MonJoueur{
 		for(Point fabrique : mapFabriqueProche.get(2)) {
 			if(estVide(fabrique) || estMaFabrique(fabrique) &&  !estRempli(fabrique) ) {
 				HashMap<Integer, ArrayList<Point>> mapObstacle = plateau.cherche(this.donnePosition(), 500,
-						Plateau.CHERCHE_TOUT);
+						Plateau.CHERCHE_FABRIQUE);
+				mapObstacle.putAll(plateau.cherche(this.donnePosition(), 500,
+						Plateau.CHERCHE_JOUEUR));
 				ArrayList<Node> pcc = plateau.donneCheminAvecObstaclesSupplementaires(this.donnePosition(),
 						fabrique, convertToNode(mapObstacle, false, false, true));
 				if(bestPcc == null && pcc != null || pcc != null && pcc.size()<bestPcc.size()) {
 					boolean ok = true;
 					for(Node node : pcc) {
 						int contenuCellule = plateau.donneContenuCellule(node.x, node.y); // joueur @1
-						if((contenuCellule & Plateau.MASQUE_PRESENCE_JOUEUR) != 0) {
+						if(contenuCellule == Plateau.ENDROIT_INFRANCHISSABLE) {
 							ok = false;
 						}
 					}
